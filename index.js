0469f63e71aca52604682fc7dbdfc164d4837b2e
Adekusibe Ogunmokun <147047093+Iamhadeh@users.noreply.github.com>
2024-01-25 09:33:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let tagLine = document.getElementById("tagline");
+
+tagLine.addEventListener("click", function (){
+    tagLine.innerHTML = ("Make your website work efficiently...");
+
+});
+
+document.querySelector("h1").addEventListener ("click", function (){
+    document.querySelector("h1").innerHTML = ("Hi, I am Adekusibe");
+    
+});
+
